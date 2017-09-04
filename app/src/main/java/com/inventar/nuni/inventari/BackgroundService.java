3a959b54923cddcbf1bde3f6cf83681fa0c56c0d
n.telo <nuni-lirda@hotmail.com>
2017-09-04 17:09:18 +0200

    download task back

--- app/src/main/java/com/inventar/nuni/inventari/BackgroundService.java
+++ app/src/main/java/com/inventar/nuni/inventari/BackgroundService.java
@@ -3,6 +3,7 @@ package com.inventar.nuni.inventari;
 import android.app.Service;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
@@ -31,38 +32,51 @@ public class BackgroundService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        DownloadTask task = new DownloadTask();
-        String res = null;
-        try {
-            res = task.execute(url_kerkuar).get();
-            String[] records = res.split(";");
-            id_db = new ArrayList<>();
-            emri_db = new ArrayList<>();
-            njesi_db = new ArrayList<>();
-            kategori_db = new ArrayList<>();
-            cmim_db = new ArrayList<>();
-            data_db = new ArrayList<>();
 
-            for (String record : records) {
-                String[] recordData = record.split(",");
-                id_db.add(recordData[0]);
-                emri_db.add(recordData[1]);
-                njesi_db.add(recordData[2]);
-                kategori_db.add(recordData[3]);
-                cmim_db.add(recordData[4]);
-                data_db.add(recordData[5]);
+
+        final CountDownTimer rifreskim = new CountDownTimer((long) 10000, 1000) {
+            public void onTick(long millisUntilFinished) {
             }
-            //mydb.fshi();
-            for (int i = 0; i < emri_db.size(); i++) {
-                mydb.shto_artikull(id_db.get(i), emri_db.get(i), njesi_db.get(i), kategori_db.get(i), cmim_db.get(i), data_db.get(i));
+
+            public void onFinish() {
+
+                DownloadTask task = new DownloadTask();
+                String res = null;
+                try {
+                    res = task.execute(url_kerkuar).get();
+                    String[] records = res.split(";");
+                    id_db = new ArrayList<>();
+                    emri_db = new ArrayList<>();
+                    njesi_db = new ArrayList<>();
+                    kategori_db = new ArrayList<>();
+                    cmim_db = new ArrayList<>();
+                    data_db = new ArrayList<>();
+
+                    for (String record : records) {
+                        String[] recordData = record.split(",");
+                        id_db.add(recordData[0]);
+                        emri_db.add(recordData[1]);
+                        njesi_db.add(recordData[2]);
+                        kategori_db.add(recordData[3]);
+                        cmim_db.add(recordData[4]);
+                        data_db.add(recordData[5]);
+                    }
+                    //mydb.fshi();
+                    for (int i = 0; i < emri_db.size(); i++) {
+                        mydb.shto_artikull(id_db.get(i), emri_db.get(i), njesi_db.get(i), kategori_db.get(i), cmim_db.get(i), data_db.get(i));
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
+
+                this.start();
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
+        }.start();
+
 
         return super.onStartCommand(intent,flags,startId);
     }
